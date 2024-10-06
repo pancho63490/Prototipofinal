d905c8ad9befa4dd662e18aec27b3ce489391f0c
pancho63490 <franciscorodriguez63490@gmail.com>
2024-10-06 13:02:36 -0600

    Initial Commit

---
+++ Prototipofinal/PrototipofinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototipofinalApp.swift
+//  Prototipofinal
+//
+//  Created by Frank Perez on 06/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PrototipofinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
