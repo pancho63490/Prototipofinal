73298247dc86d2434351b3f94601b02d5a4331f9
pancho63490 <franciscorodriguez63490@gmail.com>
2025-01-07 09:17:36 -0700

    Integracion de Verificacion de Logis ventana y navegacion de logis

---
+++ Prototipofinal/Models/Logis.swift
@@ -0,0 +1,8 @@
+//
+//  Logis.swift
+//  Prototipofinal
+//
+//  Created by Frank Perez on 28/11/24.
+//
+
+import Foundation
---
+++ Prototipofinal/Views/LogisVerificationview.swift
@@ -0,0 +1,18 @@
+//
+//  LogisVerificationview.swift
+//  Prototipofinal
+//
+//  Created by Frank Perez on 28/11/24.
+//
+
+import SwiftUI
+
+struct LogisVerificationview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogisVerificationview()
+}
