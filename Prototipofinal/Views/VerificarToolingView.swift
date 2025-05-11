0b0a9117c1c27fe53a968e1b70a6a7a80cd7bc6d
pancho63490 <franciscorodriguez63490@gmail.com>
2025-05-10 22:46:15 -0600

    version 1.0

---
+++ Prototipofinal/Views/VerificarToolingView.swift
@@ -0,0 +1,18 @@
+//
+//  VerificarToolingView.swift
+//  Prototipofinal
+//
+//  Created by Frank Perez on 21/04/25.
+//
+
+import SwiftUI
+
+struct VerificarToolingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VerificarToolingView()
+}
