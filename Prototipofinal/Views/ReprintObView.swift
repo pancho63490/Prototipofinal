c7131c6f5f0aba9b6fbba6746d641fcda12a11ff
pancho63490 <franciscorodriguez63490@gmail.com>
2025-08-12 21:38:15 -0600

    version 1.01

---
+++ Prototipofinal/Views/ReprintObView.swift
@@ -0,0 +1,18 @@
+//
+//  ReprintObView.swift
+//  Prototipofinal
+//
+//  Created by Frank Perez on 10/05/25.
+//
+
+import SwiftUI
+
+struct ReprintObView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReprintObView()
+}
